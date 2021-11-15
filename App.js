2170f75701d310844f357c4b8c44154613678064
igBFranco <igor.franco@aluno.unc.br>
2021-11-15 20:24:51 -0300

    Criação do Header com Imagem e Título Aula1

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
+
+    const image = require('./resources/bg.jpg');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex:1}}>
+      
+        <ImageBackground source={image} style={styles.image}>
+          <View style={styles.coverView}>
+            <Text style={styles.textHeader}>Lista de Tarefas</Text>
+          </View>
+        </ImageBackground>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width:'100%',
+    height:100,
+    resizeMode: "cover"
   },
+  coverView:{
+    width:'100%',
+    height:100,
+    backgroundColor:'rgba(0,0,0,0.4)'
+  },
+  textHeader:{
+    textAlign:'center',
+    color:'white',
+    fontSize:20,
+    marginTop:50,
+  }
+
 });
